1e94d2715bce7a9ae0f6c2e5955d829b1a8de495
Stuart Walsh <stu@ipng.org.uk>
2018-06-07 13:57:05 +0100

    finish alt card display for characters

--- Components/GameBoard/CardText.jsx
+++ Components/GameBoard/CardText.jsx
@@ -13,6 +13,17 @@ class CardText extends React.Component {
             }
         }
 
+        let cardText = this.props.card.text.replace(/\n/g, '<br />');
+        cardText = cardText.replace(/\[military\]/g, '<span class=\'icon-military\'/>');
+        cardText = cardText.replace(/\[intrigue\]/g, '<span class=\'icon-intrigue\'/>');
+        cardText = cardText.replace(/\[power\]/g, '<span class=\'icon-power\'/>');
+        cardText = cardText.replace(/\[baratheon\]/g, '<span class=\'icon-baratheon\'/>');
+        cardText = cardText.replace(/\[greyjoy\]/g, '<span class=\'icon-greyjoy\'/>');
+        cardText = cardText.replace(/\[thenightswatch\]/g, '<span class=\'icon-thenightswatch\'/>');
+        cardText = cardText.replace(/\[stark\]/g, '<span class=\'icon-stark\'/>');
+        cardText = cardText.replace(/\[tyrell\]/g, '<span class=\'icon-tyrell\'/>');
+        cardText = cardText.replace(/\[martell\]/g, '<span class=\'icon-martell\'/>');
+
         return (
             <div className='card-alt'>
                 <div className='card-cost'>
@@ -25,10 +36,11 @@ class CardText extends React.Component {
                 <div className='card-name-row'>
                     <div className='card-strength'>{ this.props.card.strength }</div>
                     <div className='card-name'>{ this.props.card.unique ? <span className='card-unique' /> : null } { this.props.card.name }</div>
-                    <div className='card-faction'>{ this.props.card.faction }</div>
+                    <div className={ `card-faction ${this.props.card.faction}` } />
                 </div>
                 <div className='card-text'>
-                    <span dangerouslySetInnerHTML={ {__html: this.props.card.text.replace('\n', '<br />') } }/> { /* eslint-disable-line */ }
+                    <div className='card-traits'>{ this.props.card.traits.join('. ') }.</div>
+                    <span dangerouslySetInnerHTML={ {__html: cardText } }/> { /* eslint-disable-line */ }
                 </div>
             </div>
         );
