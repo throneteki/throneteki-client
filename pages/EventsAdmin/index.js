a0fcc592f4ad4e442a9f7e7527cc4422eb3e97a8
Brian Upton <ystros@gmail.com>
2020-04-14 22:22:24 -0700

    Remove unneeded copy-paste + fix linting

--- pages/EventsAdmin/index.js
+++ pages/EventsAdmin/index.js
@@ -1,89 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import moment from 'moment';
 
 import Panel from '../../Components/Site/Panel';
-import ApiStatus from '../../Components/Site/ApiStatus';
 import * as actions from '../../actions';
-import { navigate } from '../../ReduxActions/misc';
 
 class EventsAdmin extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            ip: '',
-            currentRequest: 'REQUEST_BANLIST'
+            currentRequest: 'REQUEST_EVENTS'
         };
-
-        this.onAddBanlistClick = this.onAddBanlistClick.bind(this);
     }
 
     componentWillMount() {
         this.props.loadEvents();
     }
 
-    componentWillReceiveProps(props) {
-        let clearStatus = false;
-        if(props.banListAdded) {
-            clearStatus = true;
-            this.setState({ successMessage: 'Banlist item added successfully.' });
-        }
-
-        if(props.banListDeleted) {
-            clearStatus = true;
-            this.setState({ successMessage: 'Banlist item deleted successfully.' });
-        }
-
-        if(clearStatus) {
-            setTimeout(() => {
-                this.props.clearBanlistStatus();
-                this.setState({ successMessage: undefined });
-            }, 5000);
-        }
-    }
-
-    onIpTextChange(event) {
-        this.setState({ ip: event.target.value });
-    }
-
-    onAddBanlistClick(state) {
-        this.setState({ currentRequest: 'ADD_BANLIST' });
-        this.props.addBanlist(state.ip);
-    }
-
     handleDeleteClick(id) {
-        this.setState({ currentRequest: 'DELETE_BANLIST' });
+        this.setState({ currentRequest: 'DELETE_EVENT' });
         this.props.deleteEvent(id);
     }
 
     render() {
-        // if(this.props.apiState && this.props.apiState.loading) {
-        //     return 'Loading banlist, please wait...';
-        // }
-
-        let statusBar;
-
-        // switch(this.state.currentRequest) {
-        //     case 'REQUEST_BANLIST':
-        //         statusBar = <ApiStatus apiState={ this.props.apiState } successMessage={ this.state.successMessage } />;
-        //         break;
-        //     case 'ADD_BANLIST':
-        //         statusBar = <ApiStatus apiState={ this.props.apiAddState } successMessage={ this.state.successMessage } />;
-        //         break;
-        //     case 'DELETE_BANLIST':
-        //         statusBar = <ApiStatus apiState={ this.props.apiDeleteState } successMessage={ this.state.successMessage } />;
-        //         break;
-        // }
+        if(this.props.apiState && this.props.apiState.loading) {
+            return 'Loading events, please wait...';
+        }
 
         const { events, navigate } = this.props;
 
         return (
             <div className='col-xs-12'>
-                { statusBar }
                 <Panel title='Events administration'>
-                    <a className="btn btn-primary" href="/events/add">Add event</a>
+                    <a className='btn btn-primary' href='/events/add'>Add event</a>
                     <table className='table table-striped'>
                         <thead>
                             <tr>
@@ -110,31 +60,18 @@ class EventsAdmin extends React.Component {
 
 EventsAdmin.displayName = 'EventsAdmin';
 EventsAdmin.propTypes = {
-    addBanlist: PropTypes.func,
-    apiAddState: PropTypes.object,
-    apiDeleteState: PropTypes.object,
     apiState: PropTypes.object,
-    banListAdded: PropTypes.bool,
-    banListDeleted: PropTypes.bool,
     deleteEvent: PropTypes.func,
     events: PropTypes.array,
     loadEvents: PropTypes.func,
-    navigate: PropTypes.func,
-    clearBanlistStatus: PropTypes.func,
-    loadBanlist: PropTypes.func,
-    successMessage: PropTypes.string
+    navigate: PropTypes.func
 };
 
 function mapStateToProps(state) {
     return {
-        apiAddState: state.api.ADD_BANLIST,
-        apiDeleteState: state.api.DELETE_BANLIST,
-        apiState: state.api.REQUEST_BANLIST,
-        banlistAdded: state.admin.banlistAdded,
-        banlistDeleted: state.admin.banlistDeleted,
+        apiState: state.api.REQUEST_EVENTS,
         events: state.events.events,
-        loadBanlist: state.admin.loadBanlist,
-        loading: state.api.loading
+        loadBanlist: state.admin.loadBanlist
     };
 }
 
